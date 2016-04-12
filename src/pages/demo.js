a132d85c7632798ac69f29ffdcba6a9276fa2556
David Rios <david.rios.gomes@gmail.com>
2016-04-11 23:47:51 -0300

    single page demo and moved data-table css

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -12,4 +12,64 @@
   document
     .getElementById('menu-close')
     .addEventListener('click', menuToggleClick);
+
+  // MENU ITEMS
+  var menuListItems = menuElement.querySelectorAll('ul > li');
+
+  function handleMenuItemClick(ev) {
+    for (var i = 0; i < menuListItems.length; i++) {
+      var li = menuListItems[i];
+      var link = li.children[0];
+      if (ev.target === link) {
+        li.classList.add('active');
+      }
+      else {
+        li.classList.remove('active');
+      }
+    }
+  }
+
+  var location = window.location.toString();
+  var activeIdx = null;
+  for (var i = 0; i < menuListItems.length; i++) {
+    var li = menuListItems[i];
+    var link = li.children[0];
+
+    if (activeIdx === null) {
+      activeIdx = i;
+    }
+
+    if (li.classList.contains('active') || link.href === location) {
+      activeIdx = i;
+    }
+
+    link.dataset.position = i;
+    link.addEventListener('click', handleMenuItemClick);
+  }
+
+  var activeLink = menuListItems[activeIdx].children[0];
+  handleMenuItemClick({target: activeLink});
+  if (location !== activeLink.href) {
+    window.location = activeLink.href;
+  }
+  // END MENU ITEMS
+
+  // CONTENT
+  function handleHashChange(ev) {
+    var hash = ev.newURL.substr(ev.newURL.indexOf('#'));
+    var sections = document.querySelectorAll('body > section');
+    for (var i = 0; i < sections.length; i++) {
+      var section = sections[i];
+      if (section.dataset.link === hash) {
+        section.classList.add('active');
+      }
+      else {
+        section.classList.remove('active');
+      }
+    }
+  }
+
+  window.addEventListener('hashchange', handleHashChange);
+  handleHashChange({newURL: window.location.toString(), oldURL: null});
+  // END CONTENT
 }();
